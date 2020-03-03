4e53ff90908cc71f4211d1ad91fb81050b111240
shamuel21 <shamuelmanrrique@gmail.com>
2020-03-03 10:12:09 +0100

    feat(): fix it

--- chatapp/src/main/java/com/tmda/chatapp/controller/MyController.java
+++ chatapp/src/main/java/com/tmda/chatapp/controller/MyController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-
     @Autowired
     public MyModel model;
 
--- chatapp/src/main/java/com/tmda/chatapp/tut1/Tut1Config.java
+++
@@ -1,4 +0,0 @@
-package com.tmda.chatapp.tut1;
-
-public class Tut1Config {
-}
