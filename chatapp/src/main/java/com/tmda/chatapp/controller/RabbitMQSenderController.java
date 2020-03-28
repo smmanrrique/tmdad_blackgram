b104c652713bdbf13357140ef7496954181c5dd2
shamuel21 <shamuelmanrrique@gmail.com>
2020-03-28 16:51:23 +0100

    fic(): protobuf

--- chatapp/src/main/java/com/tmda/chatapp/controller/RabbitMQSenderController.java
+++ chatapp/src/main/java/com/tmda/chatapp/controller/RabbitMQSenderController.java
@@ -1,15 +1,12 @@
 package com.tmda.chatapp.controller;
 
-import com.tmda.chatapp.model.Message;
-import com.tmda.chatapp.model.User;
 import com.tmda.chatapp.service.RabbitMQSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/sender/")
@@ -19,23 +16,24 @@ public class RabbitMQSenderController {
     RabbitMQSender rabbitMQSender;
     private final Logger LOGGER = LoggerFactory.getLogger(RabbitMQSenderController.class);
 
-     @RequestMapping("/direct")
-//     @ResponseBody
-     public String sender(@RequestParam("sender") String fromUser, @RequestParam("receiver") String toUser, @RequestParam("body") String body) throws IOException, TimeoutException {
-//         rabbitMQSender.send(fromUser, toUser, body);
-         System.out.printf("Sender");
-         return "Message sent to the RabbitMQ JavaInUse Successfully";
-     }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Message> create(@RequestBody User user) {
-        LOGGER.info("start creating user: ", user);
-        try {
-            // User user = userService.create(user);
-            return new ResponseEntity<>(user, HttpStatus.CREATED);
-        } catch (DataAccessException e) {
-            LOGGER.info(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-        }
+    @RequestMapping("/direct")
+    // @ResponseBody
+    public String sender(@RequestParam("sender") String fromUser, @RequestParam("receiver") String toUser,
+            @RequestParam("body") String body) {
+        // rabbitMQSender.send(fromUser, toUser, body);
+        System.out.printf("Sender");
+        return "Message sent to the RabbitMQ JavaInUse Successfully";
     }
+
+    // @RequestMapping(method = RequestMethod.POST)
+    // public ResponseEntity<Message> create(@RequestBody User user) {
+    // LOGGER.info("start creating user: ", user);
+    // try {
+    // // User user = userService.create(user);
+    // return new ResponseEntity<>(user, HttpStatus.CREATED);
+    // } catch (DataAccessException e) {
+    // LOGGER.info(e.getMessage());
+    // return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+    // }
+    // }
 }
