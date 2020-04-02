ea6887191247047fd933005853fd6376f7a920b8
shamuel21 <shamuelmanrrique@gmail.com>
2020-04-02 22:06:35 +0200

    feat(): v1 RabbitMQSender

--- chatapp/src/main/java/com/tmda/chatapp/service/RabbitMQSender.java
+++ chatapp/src/main/java/com/tmda/chatapp/service/RabbitMQSender.java
@@ -9,8 +9,6 @@ import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.Connection;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -20,19 +18,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.TimeoutException;
 
 @Service
-@PropertySource("classpath:application.properties")
 public class RabbitMQSender {
-    @Value("${spring.activemq.broker-url}")
-    private String RABBITMQ_URL;
-
-    @Value("${spring.rabbitmq.host}")
-    public static String RABBITMQ_HOST;
-
-    @Value("${spring.rabbitmq.username}")
-    public static String RABBITMQ_USERNAME;
-
-    @Value("${spring.rabbitmq.password}")
-    public static String RABBITMQ_PASSWORD;
 
     private static final Logger logger = LoggerFactory.getLogger(RabbitMQSender.class.getName());
 
@@ -41,40 +27,27 @@ public class RabbitMQSender {
     private Config config;
 
 
-    public String Send(String exchange, Message message) throws IOException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException, TimeoutException {
+    public String Send(String exchange, String routingkey, Message message)
+            throws IOException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException, TimeoutException {
 
-        System.out.println("RABBITMQ_URL: "+RABBITMQ_USERNAME);
-
-        logger.info("Call connection factory ///////////////////////");
-
+        logger.info("Call connection factory into Send Function");
+//        CachingConnectionFactory connectionFactory = config.rabbitConnectionFactory();
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-//        connectionFactory.getRabbitConnectionFactory().setUri("amqp://bzwbihsx:mo3CwoHiRL6V-ZBmGqrUX0S-_2CnHVcR@hawk.rmq.cloudamqp.com/bzwbihsx");
-        connectionFactory.getRabbitConnectionFactory().setUri(RABBITMQ_URL);
-
+        connectionFactory.getRabbitConnectionFactory().setUri("amqp://bzwbihsx:mo3CwoHiRL6V-ZBmGqrUX0S-_2CnHVcR@hawk.rmq.cloudamqp.com/bzwbihsx");
         Connection connection = connectionFactory.createConnection();
 
         Channel channel = connection.createChannel(false);
 
-        String receiver = "receiver";
-        channel.queueDeclare(receiver, true, false, false, null);
+        String queueName = routingkey;
+        channel.queueDeclare(queueName, true, false, false, null);
 
-
-        channel.basicPublish("sender.receiver", "receiver", null, message.getBody().getBytes());
-        System.out.println(" [x] Enviado '" + message + "'   1 ");
-//        rabbitTemplate.convertAndSend("shamuel", "shamuel", message);
-//        rabbitTemplate.convertAndSend(exchange, routingkey, CustomMessage);
-        logger.info("Send msg to consumer= ");
-        channel.basicPublish("sender.receiver", "receiver", null, message.getBody().getBytes());
+        logger.info("Publish message");
+        channel.basicPublish(exchange, routingkey, null, message.getBody().getBytes());
         System.out.println(" [x] Enviado '" + message + "'    2");
 
         channel.close();
         connection.close();
 
-        return "Hello World" + message.getFromUser().getUserName();
-
-//        DirectExchange exchangeName = config.directExchange(exchange);
-//        Queue queue = config.queueSpecific(queueName);
-//        rabbitTemplate.convertAndSend(exchangeName, , message);
-
+        return "Send message: " + message.getFromUser().getUserName();
     }
 }
--- chatapp/src/test/java/com/tmda/chatapp/JUnitSendTest.java
+++ chatapp/src/test/java/com/tmda/chatapp/JUnitSendTest.java
@@ -2,37 +2,44 @@ package com.tmda.chatapp;
 
 
 import com.tmda.chatapp.config.Config;
+import com.tmda.chatapp.model.Message;
+import com.tmda.chatapp.model.User;
+import com.tmda.chatapp.service.RabbitMQSender;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JUnitSendTest {
 
     private Config config;
 
-//    @Test
-//    public void SendTest() throws IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
-//        String exchange = "hola";
-//        Message message = new Message();
-//        User user = new User();
-//
-//        user.setUserName("shamuel");
-//        message.setBody("22222222222");
-//        message.setFromUser(user);
-//
-//        RabbitMQSender rs =  new RabbitMQSender();
-//        System.out.println("CAll SENDER");
-//        String result = rs.Send(exchange, message);
-//        assertEquals(result, "Hello World"+user.getUserName() );
-//
-//    }
+    @Test
+    public void SendTest() throws IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
+        String exchange = "sender.receiver";
+        String receiver = "receiver";
+        Message message = new Message();
+        User user = new User();
+
+        user.setUserName("shamuel");
+        message.setBody("22222222222");
+        message.setFromUser(user);
+
+        RabbitMQSender rs =  new RabbitMQSender();
+        System.out.println("CAll SENDER");
+        String result = rs.Send(exchange, receiver, message);
+        assertEquals(result, "Send message: "+user.getUserName() );
+
+    }
 
     @Test
     public void Conn() throws IOException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
-//        Config config = new Config();
+        Config config = new Config();
         config.Proof();
 //        CachingConnectionFactory factory = config.rabbitConnectionFactory();
 //
