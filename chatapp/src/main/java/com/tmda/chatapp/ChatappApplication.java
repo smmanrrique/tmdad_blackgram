8e7389d4ee9a2e918eba2842e4e2cb5243c469c6
shamuel21 <shamuelmanrrique@gmail.com>
2020-04-09 16:58:49 +0200

    feat(): deleted old controller files

--- chatapp/src/main/java/com/tmda/chatapp/ChatappApplication.java
+++ chatapp/src/main/java/com/tmda/chatapp/ChatappApplication.java
@@ -7,18 +7,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-//@EnableConfigurationProperties(RabbitMQConfig.class)
 public class ChatappApplication implements CommandLineRunner {
-
-//    @Autowired
-//    private Environment environment;
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ChatappApplication.class, args);
     }
 
     public void run(String... args) throws Exception {
-//        System.out.println(connectionRabbitMQ.toString());
+        System.out.println("ChatappApplication");
     }
 
 }
