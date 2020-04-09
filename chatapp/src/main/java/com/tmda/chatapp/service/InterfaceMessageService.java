cb260c0b896e15208c00f7c6e33b86c77b4e080a
shamuel21 <shamuelmanrrique@gmail.com>
2020-04-09 19:10:19 +0200

    feat(): created  message service and repository

---
+++ chatapp/src/main/java/com/tmda/chatapp/service/InterfaceMessageService.java
@@ -0,0 +1,21 @@
+package com.tmda.chatapp.service;
+
+import com.tmda.chatapp.model.Message;
+
+import java.util.Optional;
+
+public interface InterfaceMessageService {
+    Message create(Message message);
+
+    Optional<Message> find(long id);
+
+    Message findByFromUser(String username);
+
+    Iterable<Message> findAll();
+
+    Message update(int id, Message user);
+
+    void delete(long id);
+
+    boolean deleteAll();
+}
---
+++ chatapp/src/main/java/com/tmda/chatapp/service/MessageService.java
@@ -0,0 +1,51 @@
+package com.tmda.chatapp.service;
+
+import com.tmda.chatapp.model.Message;
+import com.tmda.chatapp.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class MessageService implements  InterfaceMessageService {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    @Override
+    public Message create(Message message) {
+        return messageRepository.save(message);
+    }
+
+    // TODO INSERT MANY USERS SAME TIME
+    @Override
+    public Optional<Message> find(long id) {
+        return messageRepository.findById(id);
+    }
+
+    @Override
+    public Message update(int id, Message user) {
+        return null;
+    }
+
+    @Override
+    public Message findByFromUser(String username) {
+        return messageRepository.findByFromUser(username);
+    }
+
+    @Override
+    public Iterable<Message> findAll() {
+        return messageRepository.findAll();
+    }
+
+
+    @Override
+    public void delete(long id) {
+        messageRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean deleteAll() {
+        messageRepository.deleteAll();
+        return true;
+    }
+}
