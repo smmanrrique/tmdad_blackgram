3ed7e8c03f8423e7ab7674a87941b10ca30f0329
smmanrrique <shamuelmanrrique@gmail.com>
2020-05-09 12:33:27 +0200

    feat(): update group view and add table to list all groups

--- web/src/app/components/group/group.component.ts
+++ web/src/app/components/group/group.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {Group} from "./group";
+import {User} from "../auth/user-register/user";
 
 @Component({
-  selector: 'app-group',
-  templateUrl: './group.component.html',
-  styleUrls: ['./group.component.css']
+  selector: "app-group",
+  templateUrl: "./group.component.html",
+  styleUrls: ["./group.component.css"]
 })
 export class GroupComponent implements OnInit {
 
-  constructor() { }
+  selectedGroup: Group;
+
+  User = new User();
+
+  // @ts-ignore
+  groups_test: Group[] = [
+    { name: "g1", description: "ADD_USER_TO_SYSTEM", user: "[{}]" },
+    { name: "g2", description: "ADD_USER_TO_SYSTEM", user: "[]" },
+    { name: "g4", description: "ADD_USER_TO_SYSTEM", user: "[]" },
+    { name: "g3", description: "ADD_USER_TO_SYSTEM", user: "[]" },
+  ];
 
   ngOnInit() {
+
   }
 
+  constructor() { }
+
+  onSelect(group: Group): void {
+    this.selectedGroup = group;
+  }
 }
