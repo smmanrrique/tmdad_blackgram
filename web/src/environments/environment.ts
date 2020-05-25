403a22790c0d8c38cbf268eaf7bb0c2efaf3361d
smmanrrique <shamuelmanrrique@gmail.com>
2020-05-25 16:50:01 +0200

    feat(): update enviroment web

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
 	production: false,
-	// backendUrl: '104.199.11.181:8080',
-	// fileUrl: '35.195.54.72:8081',
-	backendUrl: 'http://localhost:8080',
-	fileUrl: 'http://localhost:8081'
+	backendUrl: '104.199.11.181:8080',
+	fileUrl: '35.195.54.72:8060',
+	// backendUrl: 'http://localhost:8080',
+	// fileUrl: 'http://localhost:8081'
 };
